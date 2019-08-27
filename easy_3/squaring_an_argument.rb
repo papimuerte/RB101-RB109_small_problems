8dbce7e10b826259723ef3564c7bc9a2822369cf
papimuerte <papimuerte@papimuertes-MacBook-Pro.local>
2019-08-27 11:30:07 -0600

    Easy_3

---
+++ easy_3/squaring_an_argument.rb
@@ -0,0 +1,14 @@
+#Using the multiply method from the previous assignment write a method that
+#computes the square of its argument(the square is the result of mutiplying
+#a number by itself)
+#
+def multiplier(num1, num2)
+num1 * num2
+end
+
+def number_square(num)
+  multiplier(num, num)
+end
+
+puts number_square(5)
+
