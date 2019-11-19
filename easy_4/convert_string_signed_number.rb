2eb746cee314e72d965d651000af15b23a4038af
papimuerte <nate@nateward.net>
2019-11-19 01:44:14 +0000

    Easy 4

--- easy_4/convert_string_signed_number.rb
+++ easy_4/convert_string_signed_number.rb
@@ -1,4 +1,4 @@
-def string_to_integer(string)                                                                                                      
+def string_to_integer(string)
   digit_hash = {"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9}
   total = 0 
   num_array = string.split("").reverse
@@ -16,3 +16,4 @@ def string_to_signed_integer(string)
 	when '-' then -string_to_integer(string[1..-1])
 	when '+' then string_to_integer(string[1..-1])
 end
+
